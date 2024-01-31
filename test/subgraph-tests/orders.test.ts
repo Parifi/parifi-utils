f818a94c4e41034041a75103c82dfd192effa6f1
Sudeep Biswas <sudeepb02@gmail.com>
2024-01-31 19:56:48 +0530

    Remove skip for test cases

--- test/subgraph-tests/orders.test.ts
+++ test/subgraph-tests/orders.test.ts
@@ -2,7 +2,7 @@ import { Chain, getOrderById } from '../../src';
 
 const chain = Chain.ARBITRUM_SEPOLIA;
 
-describe.skip('Order fetching logic from subgraph', () => {
+describe('Order fetching logic from subgraph', () => {
   it('should return correct order details', async () => {
     console.log('Test running successfully');
     const orderId = '0xb160ae39e7a45b21fb8f247fb11f551f996ed90d3eb9a6263e49b98827e1fc4b';
