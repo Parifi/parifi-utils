fedebe0cba2be285787f844a4bcee4afda9bbed3
Sudeep Biswas <sudeepb02@gmail.com>
2024-02-01 17:26:16 +0530

    Add function to normalize prices received from Pyth

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -5,3 +5,5 @@ export const DEVIATION_PRECISION_MULTIPLIER = new Decimal(10).pow(12);
 export const SECONDS_IN_A_YEAR = new Decimal(365 * 24 * 60 * 60);
 export const MAX_FEE = new Decimal(10000000); // 1% = 100_000
 export const WAD = new Decimal(10).pow(18); // 10^18
+export const EMPTY_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+export const PRICE_FEED_DECIMALS = 8;
--- src/pyth/index.ts
+++ src/pyth/index.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
-import { getUniqueValuesFromArray } from '../common';
+import { PRICE_FEED_DECIMALS, getUniqueValuesFromArray } from '../common';
+import Decimal from 'decimal.js';
 
 // Returns a Pyth client object based on the params provided
 export const getPythClient = async (
@@ -64,3 +65,15 @@ export async function getVaaPriceUpdateData(priceIds: string[], pythClient: Axio
   }
   return priceUpdateData.map((vaa) => '0x' + Buffer.from(vaa, 'base64').toString('hex'));
 }
+
+// Pyth currently uses different exponents for supported assets. Parifi uses all price feeds with 8 decimals
+// This function converts the price from Pyth to a format Parifi uses with 8 decimals.
+export const normalizePythPriceForParifi = (pythPrice: number, pythExponent: number): Decimal => {
+  if (pythExponent > -PRICE_FEED_DECIMALS) {
+    const adjustedFactor = new Decimal(10).pow(PRICE_FEED_DECIMALS + pythExponent);
+    return new Decimal(pythPrice).mul(adjustedFactor);
+  } else {
+    const adjustedFactor = new Decimal(10).pow(-1 * (PRICE_FEED_DECIMALS + pythExponent));
+    return new Decimal(pythPrice).div(adjustedFactor);
+  }
+};
