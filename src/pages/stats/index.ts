7be00f6915d45ff14327acfdfc4de64dcd3f3c0f
Sudeep Biswas <sudeepb02@gmail.com>
2024-02-04 17:38:03 +0530

    Add open interest functionality for stats page

--- src/pages/stats/index.ts
+++ src/pages/stats/index.ts
@@ -1,5 +1,5 @@
 import Decimal from 'decimal.js';
-import { Chain } from '../../common';
+import { Chain, DECIMAL_10, PRICE_FEED_DECIMALS } from '../../common';
 import { getBaseBorrowRatePerSecond, getDynamicBorrowRatePerSecond, getMarketSkew } from '../../contract-logic';
 import { Market } from '../../subgraph';
 
@@ -30,4 +30,28 @@ export const getMarketBorrowingRatePerHour = (
     borrowingRatePerHourLong: borrowRateLongs.mul(3600).div(new Decimal('10').pow(18)),
     borrowingRatePerHourShorts: borrowRateShorts.mul(3600).div(new Decimal('10').pow(18)),
   };
+};
+
+// Returns the total Open Interest of market for longs and shorts in formatted USD
+export const getMarketOpenInterestInUsd = (
+  market: Market,
+  normalizedMarketPrice: Decimal,
+): { openInterestInUsdLongs: Decimal; openInterestInUsdShorts: Decimal } => {
+  const totalLongs = new Decimal(market.totalLongs ?? '0');
+  const totalShorts = new Decimal(market.totalShorts ?? '0');
+
+  const marketDecimals = new Decimal(market.marketDecimals ?? '1');
+  const decimalFactor = DECIMAL_10.pow(marketDecimals);
+
+  const openInterestInUsdLongs = totalLongs
+    .mul(normalizedMarketPrice)
+    .div(decimalFactor)
+    .div(DECIMAL_10.pow(PRICE_FEED_DECIMALS));
+
+  const openInterestInUsdShorts = totalShorts
+    .mul(normalizedMarketPrice)
+    .div(decimalFactor)
+    .div(DECIMAL_10.pow(PRICE_FEED_DECIMALS));
+
+  return { openInterestInUsdLongs, openInterestInUsdShorts };
 };
