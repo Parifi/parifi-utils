d886565d7ecc9014a1e1c9702b129ef9baa1b9e6
Sudeep Biswas <sudeepb02@gmail.com>
2024-02-04 16:19:20 +0530

    Add proper error handling and return types to functions

--- src/subgraph/markets/index.ts
+++ src/subgraph/markets/index.ts
@@ -1,24 +1,37 @@
 import { request } from 'graphql-request';
 import { Chain } from '../../common/chain';
-import { getSubgraphEndpoint } from '../common';
+import { Market, getSubgraphEndpoint } from '../common';
 import { fetchAllMarketsDataQuery, fetchMarketByIdQuery } from './subgraphQueries';
 import { mapMarketsArrayToInterface, mapSingleMarketToInterface } from '../common/mapper';
 
-export const getAllMarketsFromSubgraph = async (chainId: Chain) => {
-  const subgraphEndpoint = getSubgraphEndpoint(chainId);
-  console.log('subgraphEndpoint', subgraphEndpoint);
-
-  const subgraphResponse = await request(subgraphEndpoint, fetchAllMarketsDataQuery);
-
-  return mapMarketsArrayToInterface(subgraphResponse);
+export const getAllMarketsFromSubgraph = async (chainId: Chain): Promise<Market[]> => {
+  try {
+    const subgraphEndpoint = getSubgraphEndpoint(chainId);
+    const subgraphResponse = await request(subgraphEndpoint, fetchAllMarketsDataQuery);
+    const markets = mapMarketsArrayToInterface(subgraphResponse);
+    if (markets) {
+      return markets;
+    }
+    throw new Error('Markets not found');
+  } catch (error) {
+    console.log('Markets not found');
+    throw error;
+  }
 };
 
 // Get all details of a market from subgraph by market ID
-export const getMarketById = async (chainId: Chain, marketId: string) => {
-  const subgraphEndpoint = getSubgraphEndpoint(chainId);
-
-  const formattedMarketId = marketId.toLowerCase();
-
-  const subgraphResponse: any = await request(subgraphEndpoint, fetchMarketByIdQuery(formattedMarketId));
-  return mapSingleMarketToInterface(subgraphResponse.market);
+export const getMarketById = async (chainId: Chain, marketId: string): Promise<Market> => {
+  try {
+    const subgraphEndpoint = getSubgraphEndpoint(chainId);
+    const formattedMarketId = marketId.toLowerCase();
+    const subgraphResponse: any = await request(subgraphEndpoint, fetchMarketByIdQuery(formattedMarketId));
+    const market = mapSingleMarketToInterface(subgraphResponse.market);
+    if (market && market.id === marketId) {
+      return market;
+    }
+    throw new Error('Market does not exists');
+  } catch (error) {
+    console.log('Market does not exists');
+    throw error;
+  }
 };
--- src/subgraph/orders/index.ts
+++ src/subgraph/orders/index.ts
@@ -11,11 +11,20 @@ import { mapOrdersArrayToInterface, mapSingleOrderToInterface } from '../common/
 import { EMPTY_BYTES32, getUniqueValuesFromArray } from '../../common';
 
 // Get all order by a user address
-export const getAllOrdersByUserAddress = async (chainId: Chain, userAddress: string, count: number = 10) => {
-  const subgraphEndpoint = getSubgraphEndpoint(chainId);
-
-  const subgraphResponse = await request(subgraphEndpoint, fetchOrdersByUserQuery(userAddress, count));
-  return mapOrdersArrayToInterface(subgraphResponse);
+export const getAllOrdersByUserAddress = async (
+  chainId: Chain,
+  userAddress: string,
+  count: number = 10,
+): Promise<Order[]> => {
+  try {
+    const subgraphEndpoint = getSubgraphEndpoint(chainId);
+    const subgraphResponse = await request(subgraphEndpoint, fetchOrdersByUserQuery(userAddress, count));
+    const orders = mapOrdersArrayToInterface(subgraphResponse);
+    if (!orders) throw new Error('Orders not found');
+    return orders;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Get all pending orders that are not yet settled/cancelled or expired
@@ -23,32 +32,47 @@ export const getAllPendingOrders = async (
   chainId: Chain,
   timestamp: number = Math.floor(Date.now() / 1000),
   count: number = 10,
-) => {
-  const subgraphEndpoint = getSubgraphEndpoint(chainId);
+): Promise<Order[]> => {
+  try {
+    const subgraphEndpoint = getSubgraphEndpoint(chainId);
+    const subgraphResponse = await request(subgraphEndpoint, fetchPendingOrdersQuery(timestamp, count));
 
-  const subgraphResponse = await request(subgraphEndpoint, fetchPendingOrdersQuery(timestamp, count));
-  return mapOrdersArrayToInterface(subgraphResponse);
+    const orders = mapOrdersArrayToInterface(subgraphResponse);
+    if (orders) {
+      return orders;
+    }
+    throw new Error('Orders not found');
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Get order from subgraph by order ID
-export const getOrderById = async (chainId: Chain, orderId: string) => {
-  const subgraphEndpoint = getSubgraphEndpoint(chainId);
+export const getOrderById = async (chainId: Chain, orderId: string): Promise<Order> => {
+  try {
+    const subgraphEndpoint = getSubgraphEndpoint(chainId);
+    const formattedOrderId = orderId.toLowerCase();
 
-  const formattedOrderId = orderId.toLowerCase();
-
-  const subgraphResponse: any = await request(subgraphEndpoint, fetchOrdersByIdQuery(formattedOrderId));
-  return mapSingleOrderToInterface(subgraphResponse.order);
+    const subgraphResponse: any = await request(subgraphEndpoint, fetchOrdersByIdQuery(formattedOrderId));
+    const order = mapSingleOrderToInterface(subgraphResponse.order);
+    if (order && order.id === orderId) {
+      return order;
+    }
+    throw new Error('Order does not exist');
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Get all price IDs of tokens related to the orders ids
-export const getPythPriceIdsForOrderIds = async (chainId: Chain, orderIds: string[]) => {
+export const getPythPriceIdsForOrderIds = async (chainId: Chain, orderIds: string[]): Promise<string[]> => {
+  try {
   const subgraphEndpoint = getSubgraphEndpoint(chainId);
   const formattedOrderIds: string[] = orderIds.map((orderId) => orderId.toLowerCase());
 
   const subgraphResponse: any = await request(subgraphEndpoint, fetchPriceIdsFromOrderIdsQuery(formattedOrderIds));
   const priceIds: string[] = [];
 
-  try {
     const orders: Order[] = mapOrdersArrayToInterface(subgraphResponse) || [];
     if (orders.length != 0) {
       orders.map((order) => {
