030cfb093387f3ec960c9f1695363a96f4f506d0
Shubham Kadam <39984267+shubham16kadam@users.noreply.github.com>
2024-03-11 21:38:00 +0530

    PFT-538-sdk: Bump Ref lib (#21)
    * PFT-538-sdk: Bump Ref lib
    
    * 0.0.14

--- src/subgraph/common/index.ts
+++ src/subgraph/common/index.ts
@@ -1,11 +1,7 @@
 import { Chain } from '@parifi/references';
 
 export const publicSubgraphEndpoints: { [key in Chain]: string } = {
-  [Chain.ARBITRUM_GOERLI]: 'https://api.thegraph.com/subgraphs/name/parifi/parifi-arbitrum',
   [Chain.ARBITRUM_SEPOLIA]: 'https://api.thegraph.com/subgraphs/name/parifi/parifi-sepolia',
-  [Chain.ARBITRUM]: '',
-  [Chain.POLYGON]: '',
-  [Chain.BASE]: '',
 };
 
 export const getPublicSubgraphEndpoint = (chainId: Chain) => {
