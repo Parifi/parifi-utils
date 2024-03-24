f21498e1b30d1cc90def376711b7587d4e8648c3
Sudeep Biswas <sudeepb02@gmail.com>
2024-03-25 00:13:20 +0530

    Get realized PNL for user address

---
+++ test/subgraph-tests/accounts.test.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import { Chain } from '@parifi/references';
+import { ParifiSdk } from '../../src';
+import { PythConfig, RelayerConfig, RelayerI, RpcConfig, SubgraphConfig } from '../../src/interfaces/classConfigs';
+
+const rpcConfig: RpcConfig = {
+  chainId: Chain.ARBITRUM_SEPOLIA,
+};
+
+const subgraphConfig: SubgraphConfig = {
+  subgraphEndpoint: 'https://api.studio.thegraph.com/query/68480/parifi-arb-sepolia-test-dev/v0.0.6',
+};
+
+const pythConfig: PythConfig = {
+  pythEndpoint: process.env.PYTH_SERVICE_ENDPOINT,
+  username: process.env.PYTH_SERVICE_USERNAME,
+  password: process.env.PYTH_SERVICE_PASSWORD,
+  isStable: true,
+};
+
+const gelatoConfig: RelayerI = {
+  apiKey: process.env.GELATO_KEY,
+};
+
+const relayerConfig: RelayerConfig = {
+  gelatoConfig: gelatoConfig,
+};
+
+const parifiSdk = new ParifiSdk(rpcConfig, subgraphConfig, relayerConfig, pythConfig);
+
+describe('Order fetching logic from subgraph', () => {
+  it('should return PNL details for a user', async () => {
+    await parifiSdk.init();
+
+    // Use an address with a non-zero positions/deposits
+    const userAddress = '0xe4fDB1Fa65b29533D6d3D9Aa74e07E6e87405B32';
+
+    const { totalRealizedPnlPositions, totalRealizedPnlVaults } =
+      await parifiSdk.subgraph.getRealizedPnlForUser(userAddress);
+
+    console.log('totalRealizedPnlPositions', totalRealizedPnlPositions);
+    console.log('totalRealizedPnlVaults', totalRealizedPnlVaults);
+
+    const unrealizedPNL = await parifiSdk.subgraph.getTotalUnrealizedPnlInUsd(userAddress);
+    console.log("unrealizedPNL", unrealizedPNL)
+  });
+});
--- test/subgraph-tests/orders.test.ts
+++ test/subgraph-tests/orders.test.ts
@@ -1,20 +1,67 @@
+import 'dotenv/config';
+
 import { Chain } from '@parifi/references';
 import { ParifiSdk } from '../../src';
-import { RpcConfig } from '../../src/interfaces/classConfigs';
-
+import { PythConfig, RelayerConfig, RelayerI, RpcConfig, SubgraphConfig } from '../../src/interfaces/classConfigs';
 const rpcConfig: RpcConfig = {
   chainId: Chain.ARBITRUM_SEPOLIA,
 };
 
-const parifiSdk = new ParifiSdk(rpcConfig, {}, {}, {});
+const subgraphConfig: SubgraphConfig = {
+  subgraphEndpoint: 'https://api.studio.thegraph.com/query/68480/parifi-arb-sepolia-test-dev/v0.0.6',
+};
+
+const pythConfig: PythConfig = {
+  pythEndpoint: process.env.PYTH_SERVICE_ENDPOINT,
+  username: process.env.PYTH_SERVICE_USERNAME,
+  password: process.env.PYTH_SERVICE_PASSWORD,
+  isStable: true,
+};
+
+const gelatoConfig: RelayerI = {
+  apiKey: process.env.GELATO_KEY,
+};
+
+const relayerConfig: RelayerConfig = {
+  gelatoConfig: gelatoConfig,
+};
+
+const parifiSdk = new ParifiSdk(rpcConfig, subgraphConfig, relayerConfig, pythConfig);
 
 describe('Order fetching logic from subgraph', () => {
   it('should return correct order details', async () => {
     await parifiSdk.init();
-    const orderId = '0xb160ae39e7a45b21fb8f247fb11f551f996ed90d3eb9a6263e49b98827e1fc4b';
+    const orderId = '0xdede011c078916f11a1c92808d527f06db18da40d61e0eb05dc13a8d4e65447f';
 
     const order = await parifiSdk.subgraph.getOrderById(orderId);
     console.log(order);
     expect(order.id).toBe(orderId);
+
+    const priceIds = await parifiSdk.subgraph.getPythPriceIdsForOrderIds([orderId]);
+    const collateralPriceIds = await parifiSdk.pyth.getPriceIdsForCollaterals();
+
+    console.log('PRice ids : ', priceIds.concat(collateralPriceIds));
+
+    const priceUpdateData = await parifiSdk.pyth.getVaaPriceUpdateData(priceIds.concat(collateralPriceIds));
+    console.log('price update data', priceUpdateData);
+  });
+
+  it('should settle order using Gelato', async () => {
+    await parifiSdk.init();
+    const orderId = '0x8e96c0d38e6d09593c0e96457d2681b62f208ca4a2c3f86fdbf47a6cff0c3dd1';
+
+    const order = await parifiSdk.subgraph.getOrderById(orderId);
+    expect(order.id).toBe(orderId);
+
+    const priceIds = await parifiSdk.subgraph.getPythPriceIdsForOrderIds([orderId]);
+    const collateralPriceIds = await parifiSdk.pyth.getPriceIdsForCollaterals();
+
+    console.log('Price ids : ', priceIds.concat(collateralPriceIds));
+
+    const priceUpdateData = await parifiSdk.pyth.getVaaPriceUpdateData(priceIds.concat(collateralPriceIds));
+    console.log('price update data', priceUpdateData);
+
+    const { gelatoTaskId: taskId } = await parifiSdk.core.settleOrderUsingGelato(orderId);
+    console.log('taskId', taskId);
   });
 });
