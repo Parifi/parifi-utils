013bbdd487cc6592916a7ff9fb56847f5fbe261d
racso2609 <oscaremiliolugo@gmail.com>
2024-03-26 16:59:26 -0400

    add arbitrum

--- src/subgraph/common/index.ts
+++ src/subgraph/common/index.ts
@@ -2,6 +2,7 @@ import { Chain } from '@parifi/references';
 
 export const publicSubgraphEndpoints: { [key in Chain]: string } = {
   [Chain.ARBITRUM_SEPOLIA]: 'https://api.thegraph.com/subgraphs/name/parifi/parifi-sepolia',
+  [Chain.ARBITRUM_MAINNET]: 'https://subgraph.satsuma-prod.com/ac10c1d41dcb/parifis-team--3804602/parifi-arbitrum/api',
 };
 
 export const getPublicSubgraphEndpoint = (chainId: Chain) => {
