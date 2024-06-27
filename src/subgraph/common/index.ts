fd0ef25b973bf01357d785af41cb168413e54720
Amit Kumar <iamamitkumar051@gmail.com>
2024-06-27 10:06:36 +0530

    change subgraph endpoint

--- src/subgraph/common/index.ts
+++ src/subgraph/common/index.ts
@@ -2,7 +2,8 @@ import { Chain } from '@parifi/references';
 
 export const publicSubgraphEndpoints: { [key in Chain]: string } = {
   [Chain.ARBITRUM_SEPOLIA]: 'https://api.thegraph.com/subgraphs/name/parifi/parifi-sepolia',
-  [Chain.ARBITRUM_MAINNET]: 'https://subgraph.satsuma-prod.com/ac10c1d41dcb/parifis-team--3804602/parifi-arbitrum/api',
+  [Chain.ARBITRUM_MAINNET]:
+    'https://subgraph.satsuma-prod.com/ac10c1d41dcb/parifis-team--3804602/parifi-arbitrum/version/1.0.0/api',
 };
 
 export const getPublicSubgraphEndpoint = (chainId: Chain) => {
