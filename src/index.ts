c7bd1bce0b463b45282063b04fe1c1c044f3ebe6
Akhilleshgoswami <agiri5375@gmail.com>
2024-10-23 13:17:30 +0530

    add pimlico

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Gelato } from './relayers/gelato';
 import { relayerRepository } from './interfaces/repositories/relayer';
 import { Perps } from './perps';
 import { Core } from './core';
+import { Pimlico } from './relayers';
 
 export * from './common';
 export * from './relayers/gelato/gelato-function';
@@ -20,6 +21,7 @@ export class ParifiSdk {
   core: Core;
   relayer: {
     gelato: Gelato;
+    pimlico:Pimlico
   };
 
   constructor(
@@ -33,6 +35,7 @@ export class ParifiSdk {
     this.perps = new Perps();
     this.relayer = {
       gelato: new Gelato(relayerConfig['gelatoConfig'], rpcConfig),
+      pimlico: new Pimlico(relayerConfig['pimlicoConfig'], rpcConfig, subgraphConfig),
     };
     this.core = new Core();
   }
