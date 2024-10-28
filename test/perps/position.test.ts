4fed570f335ee4a23158ca64f6781dce646091be
Akhilleshgoswami <agiri5375@gmail.com>
2024-10-28 12:47:12 +0530

    fixed order type

--- test/perps/position.test.ts
+++ test/perps/position.test.ts
@@ -15,7 +15,7 @@ describe('check Pnl value', () => {
     const {totalProfitOrLoss} =  parifiSdk.perps.getProfitOrLossInUsd(new Decimal(1150), new Decimal(1190000000000000000000), new Decimal(-5000000000000000000), 18);
     expect(totalProfitOrLoss).toEqual(new Decimal(200))
   });
-  it('should return right pnl for short position', async () => {
+  it('should return right pnl for Long position', async () => {
     if (!parifiSdk) throw new Error('Parifi SDK not initialized');
     const {totalProfitOrLoss} =  parifiSdk.perps.getProfitOrLossInUsd(new Decimal(1250), new Decimal(1190000000000000000000), new Decimal(5000000000000000000), 18);
     expect(totalProfitOrLoss).toEqual(new Decimal(300))
