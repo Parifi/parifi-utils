5c5457dfe272db960c4d5ab530b6ea84f7f89286
Amit Kumar <iamamitkumar051@gmail.com>
2024-11-15 10:39:41 +0530

    added function to check user is active or not

--- src/subgraph/index.ts
+++ src/subgraph/index.ts
@@ -27,6 +27,7 @@ import { getPublicSubgraphEndpoint } from './common';
 import { Pyth } from '../pyth';
 import Decimal from 'decimal.js';
 import {
+  checkisUserActive,
   getAccountByAddress,
   getFeesByAddress,
   getLeaderboardUserData,
@@ -246,6 +247,12 @@ export class Subgraph {
     const subgraphEndpoint = this.getSubgraphEndpoint(this.rpcConfig.chainId);
     return getFeesByAddress(subgraphEndpoint, userAddresses);
   }
+
+  public async checkisUserActive(userAddress: string) {
+    const subgraphEndpoint = this.getSubgraphEndpoint(this.rpcConfig.chainId);
+    return checkisUserActive(subgraphEndpoint, userAddress);
+  }
+
   public async getExecutionFee(): Promise<{ executionFeeEth: Decimal; executionFeeUsdc: Decimal }> {
     const subgraphEndpoint = this.getSubgraphEndpoint(this.rpcConfig.chainId);
     return getExecutionFee(subgraphEndpoint);
